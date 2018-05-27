11fcac09ed4caa2aace3475f863ce0894a8c6433
Tareq3 <mti.tareq3@gmail.com>
2018-05-28 00:17:58 +0600

    Initial branch_02 commit

--- app/src/main/java/com/mti/coordinated_motion_animation/MainActivity.java
+++ app/src/main/java/com/mti/coordinated_motion_animation/MainActivity.java
@@ -2,6 +2,16 @@ package com.mti.coordinated_motion_animation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
+
+import java.util.Random;
+
+ /*
+  **activity_main + Card view in gradle
+  * */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        animateViewsIn();
+    }
+    private void animateViewsIn() {
+        // setup random initial state
+        ViewGroup root = (ViewGroup) findViewById(R.id.root);
+        float maxWidthOffset = 2f * getResources().getDisplayMetrics().widthPixels;
+        float maxHeightOffset = 2f * getResources().getDisplayMetrics().heightPixels;
+        Interpolator interpolator =
+                AnimationUtils.loadInterpolator(this, android.R.interpolator.linear_out_slow_in);
+        Random random = new Random();
+        int count = root.getChildCount();
+        for (int i = 0; i < count; i++) {
+            View view = root.getChildAt(i);
+            view.setVisibility(View.VISIBLE); //at Start
+            view.setAlpha(0.85f); //at start
+            float xOffset = random.nextFloat() * maxWidthOffset;
+            if (random.nextBoolean()) {
+                xOffset *= -1;
+            }
+            view.setTranslationX(xOffset); //setting random pos at Start
+            float yOffset = random.nextFloat() * maxHeightOffset;
+            if (random.nextBoolean()) {
+                yOffset *= -1;
+            }
+            view.setTranslationY(yOffset); //setting random pos at Start
+
+            // now animate them back into their natural position
+            view.animate()
+                    .translationY(0f) //at end
+                    .translationX(0f)
+                    .alpha(1f)
+                    .setInterpolator(interpolator)
+                    .setDuration(1000)
+                    .start();
+        }
+    }
+
+    public void containerClick(View view) {
+        animateViewsIn(); //change this line with your code
     }
 }
